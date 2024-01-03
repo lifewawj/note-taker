b5347be0c508becfce3d9842243eb202a9ee28bf
lifewawj <arevaloaj01@gmail.com>
2024-01-02 21:06:13 -0800

    app.post request is ADDED, along with comments to better understand the code

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 //The Port the server is going to run on
 const PORT = 3001;
@@ -10,12 +11,14 @@ const PORT = 3001;
 const app = express();
 
 // Sets up the middleware
+// Targets using the public folder content
 app.use(express.static('public'));
+// Takes in the users inputed data from any HTTP request and JSON parses it
+app.use(express.json());
 
-// Sets up our server to listen for http requests
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
-);
+
+
+
 
 // GET Route for the "/" homepage, when the "Get Started" button is clicked it will take you to the "/notes" page
 app.get('/notes', (req, res) => {
@@ -29,21 +32,68 @@ app.get('/', (req, res) => {
 
 // GET Route to retrieve the notes data in db.json file
 app.get('/api/notes', (req, res) => {
-  const noteData = './db/db.json'
-  fs.readFile(noteData, "utf8", (err, data) => {
+  const noteData = './db/db.json';
+  fs.readFile(noteData, 'utf8', (err, data) => {
     if (err) {
-      return console.log(err);
+      console.log(err)
+      return res.status(500).send('Server Error');
     } else {
       res.json(JSON.parse(data));
     }
   });
 });
 
-// TODO: 
-// Create a app.post route for User to ADD new notes to the db.json file
-// app.post('/api/notes', (req, res) => {
-// })
+
+// Create an app.post route for users to add new notes to the db.json file
+app.post('/api/notes', (req, res) => {
+  // Stores the user's inputed req into the title and text
+  const { title, text } = req.body;
+  // Created a const variable for the location of the database file as a string
+  const noteData = './db/db.json';
+
+  // Read the noteData to make sure we get the current one
+  fs.readFile(noteData, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Server Error');
+    }
+
+    // Store the current noteData into a const var
+    const currentNotes = JSON.parse(data);
+
+    // Create a const variable to store the newNote in
+    const newNote = {
+      title,
+      text,
+      // Using the NPM package for ids
+      id: uuidv4(),
+    };
+
+    // Gets a copy of the currentNotes, and combines it with the newNote, creating a new Array called updatedNotes
+    const updatedNotes = [...currentNotes, newNote];
+
+    // With the updatedNotes we writeFile to the noteData file, JSON.stringifying the updatedNotes
+    fs.writeFile(noteData, JSON.stringify(updatedNotes), 'utf8', (err) => {
+      // If there is an error, return the error.
+      if (err) {
+        console.error(err);
+        return res.status(500).send('Server Error');
+      } else {
+        // If it successfully writes return the updatedNotes
+        console.log('Notes successfully Added and Updated!')
+        return res.json(updatedNotes);
+      }
+    });
+  });
+});
+
+
 
 // Create a app.delete route for the User to DELETES notes from the db.json file
 // app.delete('/api/notes', (req, res) => {
 // })
+
+// Sets up our server to listen for http requests
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
